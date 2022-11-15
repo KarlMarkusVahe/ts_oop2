3c2774288691588e4fc7bc53fdc0ca39edf3a8f9
KarlMarkus <karl-markus.vaher@voco.ee>
2022-11-15 14:48:48 +0200

    UI done

---
+++ calory_tracker/app.js
@@ -0,0 +1,23 @@
+const  ItemController = (function (){
+    let data = []
+
+    function add(item){
+        data.push(item)
+        console.log('Item is added')
+    }
+
+    function get(id){
+        return data.find(item => {
+            return item.id === id
+        })
+    }
+
+    return {
+        add: add,
+        get: get
+    }
+
+})();
+
+ItemController.add({id: 1, name: 'Kate'})
+console.log(ItemController.get(1))
