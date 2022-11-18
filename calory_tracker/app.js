594db94d86f06458259ed0c7ec0bd52d8bc39848
KarlMarkus <karl-markus.vaher@voco.ee>
2022-11-18 12:40:22 +0200

    Added item to data structure

--- calory_tracker/app.js
+++ calory_tracker/app.js
@@ -18,6 +18,19 @@ const itemCtrl = (function (){
         getItems: function (){
             return data.items
         },
+        addItem: function (name, calories){
+            let ID;
+            if (data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1
+                console.log(ID)
+            } else {
+                ID = 0
+            }
+            calories = parseInt(calories);
+            newItem = new Item(ID, name, calories);
+            data.items.push(newItem);
+            return newItem
+        },
         logData: function (){
             return data
         }
@@ -25,6 +38,12 @@ const itemCtrl = (function (){
 })();
 
 const UICtrl = (function (){
+    const UISelectors = {
+        itemList: '#item-list',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories',
+        addBtn: '.add-btn'
+    }
     return {
         populateItemList: function (items){
             let html = '';
@@ -33,17 +52,41 @@ const UICtrl = (function (){
                 html += `<li class="collection-item" id="item-${item.id}"><strong>${item.name}:</strong><em>${item.calories} Calories</em><a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a></li>`;
 
             });
-            document.querySelector('#item-list').innerHTML = html
+            document.querySelector(UISelectors.itemList).innerHTML = html
+        },
+        getSelectors: function (){
+            return UISelectors;
+        },
+        getItemInput: function (){
+            return {
+                name:document.querySelector(UISelectors.itemNameInput).value,
+                calories:document.querySelector(UISelectors.itemCaloriesInput).value
+            }
         }
     }
 })();
 
 const App = (function (itemCtrl, UICtrl){
+    const loadEventListeners = function (){
+        const UISelectors = UICtrl.getSelectors();
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+    }
+
+    const itemAddSubmit = function (event){
+        const input = UICtrl.getItemInput()
+        if (input.name !== '' && input.calories !== ''){
+            const newItem = itemCtrl.addItem(input.name, input.calories)
+        }
+        event.preventDefault()
+    }
+
     return {
         init: function (){
             const items = itemCtrl.getItems()
             console.log(items)
             UICtrl.populateItemList(items)
+            loadEventListeners();
         }
     }
 })(itemCtrl, UICtrl);
